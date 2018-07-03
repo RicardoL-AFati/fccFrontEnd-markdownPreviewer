f3589235d4b58ae45f1ed4a5851b4063fac31e24
RicardoL-AFati <rickydem24@gmail.com>
2018-07-03 15:39:06 -0600

    updated styling and added break option to marked function

--- src/components/previewer.js
+++ src/components/previewer.js
@@ -1,17 +1,86 @@
 import React, { Component } from 'react';
+import marked from 'marked';
 
 export default class Previewer extends Component {
+  componentDidMount() {
+    this.setState({ html: marked(this.state.markdown, { breaks: true, })});
+  }
   constructor(props) {
     super(props);
 
-    this.state = { value: ''};
-  }
+    this.state = { 
+      markdown: 
+`# Welcome to my React Markdown Previewer!
 
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`,
+      html: '',
+    };
+  }
+  
+  handleChange(value) {
+    this.setState({ markdown: value, html: marked(value, { breaks: true, }), });
+  }
+  
   render() {
     return (
-      <textarea id='editor'
-        value={this.state.value}
-        onChange={event => this.setState({ value: event.target.value})} />
+    <div className='wrapper'>
+        <div className='editor'>
+            <h6 className='bio'>Coded by <a href='https://github.com/RicardoL-AFati'>Ricardo Ledesma</a></h6>
+            <label htmlFor='editor'>Editor</label>
+            <br />
+            <textarea id='editor' rows='30' cols='50'
+                value={this.state.markdown}
+                onChange={event => this.handleChange(event.target.value)} />
+        </div>
+        <div className='previewDiv'>
+            <label>Previewer</label>
+            <br />
+            <div dangerouslySetInnerHTML={{__html: this.state.html}} id='preview'></div>
+        </div>
+    </div>
     );
   }
 }
